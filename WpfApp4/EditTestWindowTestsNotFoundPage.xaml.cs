67ae59871e078426033bc088eec4f06e234d2bc0
RedmiBook <RedmiBook@KROLIK>
2024-09-23 01:31:53 +0300

    Добавьте файлы проекта.

---
+++ WpfApp4/EditTestWindowTestsNotFoundPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp4
+{
+    public partial class EditTestWindowTestsNotFoundPage : Page
+    {
+        public EditTestWindowTestsNotFoundPage()
+        {
+            InitializeComponent();
+            TextBlock textBlock1 = new TextBlock();
+            textBlock1.Text = "аааа";
+            textBlock1.Opacity = 0.3;
+            textBlock1.Foreground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("black"));
+            textBlock1.HorizontalAlignment = HorizontalAlignment.Center;
+            textBlock1.VerticalAlignment = VerticalAlignment.Center;
+            textBlock1.FontFamily = new FontFamily("Моя");
+            textBlock1.FontSize = 86;
+            textBlock1.FontWeight = FontWeights.Bold;
+            MainSpace.Children.Add(textBlock1);
+
+            TextBlock textBlock2 = new TextBlock();
+            textBlock2.Text = "Пока что нет вопроса, на который можно ответить.\nПодождите пока будут вопросы.";
+            textBlock2.HorizontalAlignment = HorizontalAlignment.Center;
+            textBlock2.VerticalAlignment = VerticalAlignment.Center;
+            textBlock2.TextAlignment = TextAlignment.Center;
+            textBlock2.FontFamily = new FontFamily("Моя");
+            textBlock2.Width = 450;
+            textBlock2.FontSize = 16;
+            textBlock2.TextWrapping = TextWrapping.Wrap;
+            MainSpace.Children.Add(textBlock2);
+        }
+    }
+}
---
+++ WpfApp4/Test.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp4
+{
+    public class Test
+    {
+        public enum Answers
+        {
+            First,
+            Second,
+            Third
+        }
+        public Test(string name, string description, string firstAnswer, string secondAnswer, string thirdAnswer)
+        {
+            Name = name;
+            Description = description;
+            FirstAnswer = firstAnswer;
+            SecondAnswer = secondAnswer;
+            ThirdAnswer = thirdAnswer;
+
+        }
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string FirstAnswer { get; set; }
+        public string SecondAnswer { get; set; }
+        public string ThirdAnswer { get; set; }
+        public Answers RightAnswer { get; set; }
+    }
+}
